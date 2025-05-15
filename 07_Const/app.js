e44566f3f4cbdcc1d19359cba3491ab92da8c4b6
lawrindovsk <gustavo.laurindo.santos@gmail.com>
2025-05-15 00:48:37 -0300

    learning more

---
+++ 07_Const/app.js
@@ -0,0 +1,7 @@
+//var
+const nome = "gustavo";
+
+nome = "antonio";
+
+console.clear();
+console.log(nome);
---
+++ 08_Datatypes/intro.js
@@ -0,0 +1,12 @@
+// data types
+/*
+number
+bigInt
+String
+Boolean
+Null value
+Undefined value
+Objects
+Simbols
+*/
+
